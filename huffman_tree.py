c1ed98bda1dbbd34baf71da0793de8f8cb7ad576
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2024-11-30 15:31:17 +0200

    Huffman code fix

--- huffman_tree.py
+++ huffman_tree.py
@@ -28,7 +28,7 @@ def ConstructHuffmanTree(bytePopularity: Dict[str, int]) -> Node:
     root: Node = leafs.pop(0)
     return root
 
-def ConstructHuffmanCode(node: Node, coding: Dict[str, int], currentCode: chr = 0) -> None:
+def ConstructHuffmanCode(node: Node, coding: Dict[str, int], currentCode: chr = 1) -> None:
     left, right = node.m_left, node.m_right
     
     if left != None:
@@ -45,13 +45,37 @@ if __name__ == "__main__":
         "A": 4,
         "B": 2,
         "C": 3,
-        "D": 1
+        "D": 1,
+        "E": 6,
+        "F": 3,
+        "G": 9,
+        "H": 4,
+        "I": 2,
+        "J": 7,
+        "J": 3,
+        "K": 10,
+        "L": 11,
+        "M": 9,
+        "N": 1,
+        "O": 3,
+        "P": 2,
+        "Q": 5,
+        "R": 4,
+        "S": 7,
+        "T": 8,
+        "U": 6,
+        "V": 1,
+        "T": 2,
+        # "W": 6,
+        # "X": 9,
+        # "Y": 8,
+        # "Z": 1
     }
     
     root: Node = ConstructHuffmanTree(bytePopularity)
     huffmanCode: Dict[str, int] = {}
     ConstructHuffmanCode(root, huffmanCode)
     
-    for item in huffmanCode.items():
-        byte, code = item[0], item[1]
+    for byte in sorted(huffmanCode.keys()):
+        code = huffmanCode[byte]
         print(f"{byte}: {bin(code)}")
