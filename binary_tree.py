213bd5491768ac9a92000cd16233d6177d1659ca
Oskaras Vištorskis <89640069+Soskar1@users.noreply.github.com>
2024-11-30 13:08:31 +0200

    Added __future import

--- binary_tree.py
+++ binary_tree.py
@@ -1,12 +1,14 @@
+from __future__ import annotations
+
 class Node(object):
     def __init__(self):
-        self.m_left: 'Node' = None
-        self.m_right: 'Node' = Node
+        self.m_left: Node = None
+        self.m_right: Node = Node
         self.m_code: int = 0
         self.m_byte: int = 0
 
-    def AddRight(self, node: 'Node') -> None:
+    def AddRight(self, node: Node) -> None:
         self.m_right = node
     
-    def AddLeft(self, node: 'Node') -> None:
+    def AddLeft(self, node: Node) -> None:
         self.m_left = node
